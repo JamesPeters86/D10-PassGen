7712e57558a7340499b0f21d7985cae973cd2d53
James Peters <32214004+JamesPeters86@users.noreply.github.com>
2025-05-21 06:52:59 -0600

    Shortened/Simplified code. Clarified comments.
    Removed the option to specify filename as a command line argument. The filename can be changed in the program if this is truly desired. 
    
    Switched to loading the entire file into a list rather than rereading it line by line for every token. Seriously, was I drunk when I originally wrote this?
    
    Removed some unnecessary code that was a holdover from utilities.

--- passGen.py
+++ passGen.py
@@ -1,60 +1,40 @@
 #!/usr/bin/python3
-import sys
-filename = "tokenList"
-if len(sys.argv) == 2:
-	filename = sys.argv[1]
 
-fptr = open(filename, "r")
+with open("tokenList") as fptr:
+    lines = fptr.readlines()
 
-#count the lines and lengths
-cnt = 0
-lengs = [0] * 40
-while True:
-
-	line = fptr.readline()
-	if "" == line:
-		break
-	cnt += 1
-
-#We want an even probablity distribution. 
-#This will give 3 numbers that can generate each token
-#For example the first token will be picked by 0, count, 2 * count
-#The second token will bepickedgen by 1, count + 1, and 2 * count + 1
-#If we don't throw out rolls that are higher than 3 * count,
-#the first several tokens will have 4 numbers that pick and thus will 
-#have a 33% higher probablity of being picked. 
+# We want an even probablity distribution. 
+# Rolling the dice 4 times will give 3 numbers that can select each token.
+# For example, the vales: 0, count, and 2 * count will select the first token in the file.
+# The vales: 1, count + 1, and 2 * count + 1  will select the second token in the file.
+# If we don't throw out rolls that are higher than 3 * count, the first several tokens will
+# have 4 numbers that select them. Thus they will have a 33% higher probablity of being picked. 
+cnt = len(lines)
 maxRoll = cnt * 3 - 1
 
 print("There are " + str(cnt) + " different tokens")
 if cnt > 3333:
-	print("Only the first 3333 tokens will be used.")
-	cnt = 3333
+    print("Only the first 3333 tokens will be used.")
+    cnt = 3333
 print("For each token, roll a d10 4 times. For 10 enter 0.")
 print("If your number is greater than", maxRoll, "start over from the first roll")
 print("Enter q when done");
-print("Total Digits\t","Token")
+print("Total Digits\t", "Token")
 
-#print the tokens along with a running count of the digits
+# print the tokens along with a running count of the digits
 digitCount = 0
 while True:
-	line = input("Number: ").strip()
-	if "q" == line:
-		break
+    line = input("Number: ").strip()
+    if "q" == line:
+        break
 
-	number = int(line);
-	if number > maxRoll:
-		print("Too big, retry!")
-		continue
+    number = int(line);
+    if number > maxRoll:
+        print("Too big, retry!")
+        continue
 
-	# use modulo to wrap back around the list
-	number = (number % cnt) + 1
-	fptr.seek(0)
-	idx = 0
-	while idx < number:
-		line = fptr.readline()
-		idx += 1
-
-	line = line.strip()
-	digitCount += len(line)
-	print(digitCount, "\t\t", line)
-
+    # use modulo to wrap back around the list
+    number = (number % cnt)
+    token = lines[number].strip()
+    digitCount += len(token)
+    print(digitCount, "\t\t", token)
